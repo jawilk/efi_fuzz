cafcd658a2dfc6b79ba6c6cbabf4ade7212ee1be
wj <wzggUaPFt5J7f@gmx.de>
2023-06-07 20:59:55 +0200

    Cleanup

--- core/EmulationManager.py
+++ core/EmulationManager.py
@@ -30,22 +30,22 @@ class EmulationManager:
             extra_modules = []
 
         self.ql = Qiling(extra_modules + [target_module],
-                         ".", verbose=QL_VERBOSE.DEBUG)  # ,                                      # rootfs
-        # output="trace")
+                         ".", verbose=QL_VERBOSE.DEBUG)
 
         # callbacks.init_callbacks(self.ql)
 
         self.coverage_file = None
 
         self.sanitizers = EmulationManager.DEFAULT_SANITIZERS
-        self.fault_handler = 'exit'  # By default we prefer to exit the emulation cleanly
-        self.fat_image = None
+        self.fault_handler = 'exit'
+
+        # Load fat image into the env
         self.load_fat_image(
             "/home/wj/temp/uefi/test-fat/dummy_esp.img")
 
     def load_fat_image(self, path):
         with open(path, "rb") as file:
-            self.fat_image = file.read()
+            self.ql.env["FAT_META"] = file.read()
 
     def load_nvram(self, nvram_file):
         # Load NVRAM environment.
@@ -124,96 +124,93 @@ class EmulationManager:
         elif value == 'break':
             self.ql.os.fault_handler = fault._break
 
-    def hi2(self, m):
-        print("ENDHEREEEEEEEEEEEEEEEEEEEEE")
+    def fat_binding_start(self, m):
+        print("!"*10, "FAT DRIVER BINDING START")
         self.ql.os.emit_context()
         # print(hex(self.ql.arch.regs.arch_pc))
 
-    def hi3(self, m):
-        print("!!!!!!!!!!!!!!! On call FatOpenDevice")
+    def fat_open_device(self, m):
+        print("!"*10, "On call FatOpenDevice")
 
-    def hi4(self, m):
-        print("!!!!!!!!!!!!!!! After call FatAllocateVolume")
+    def fat_allocate_volume(self, m):
+        print("!"*10, "After call FatAllocateVolume")
 
-    def hi5(self, m):
-        # addr start in r8
-        # size param is in r9
-        # &buf pointer is in RSP+0x28
+    def disk_io_read_blocks(self, m):
+        """Mock DiskIo through hooking into the FakeController FakeControllerReadDisk func
+        offset in r8
+        size param is in r9
+        &buf pointer is in RSP+0x28
+        """
+        print("!"*10, "START DISKIO READ BLOCKS")
 
-        print("!!!!!!!!!!!!!!! DISK IO READ BLOCKS")
+        # Fetch func args
         offset = self.ql.arch.regs.r8
         size = self.ql.arch.regs.r9
         buf_addr = self.ql.mem.read(
             self.ql.arch.regs.rsp + 0x28, 8)
         buf_addr = int.from_bytes(buf_addr, 'little')
-        # self.ql.mem.read(self.ql.arch.stack_read(
-        # 5 * self.ql.arch.pointersize), 8)
+
         print("offset:", hex(offset))
         print("size:", size)
         print("buf addr:", buf_addr, hex(buf_addr))
-        self.ql.os.emit_context()
-        self.ql.os.emit_stack(12)
-        pc = self.ql.arch.regs.arch_pc
-        data = self.ql.mem.read(pc, size=64)
-        self.ql.os.emit_disasm(pc, data, 32)
-        # print(self.fat_image[:size])
-        # bytes([1, 2, 3, 4]))
-        print("data:", self.fat_image[offset:offset+size])
-        self.ql.mem.write(buf_addr, self.fat_image[offset:offset+size])
-        self.ql.os.emit_stack(12)
-        print("HEREEEEEE AFTER STACK WRITE")
+        # self.ql.os.emit_stack(12)
+        # pc = self.ql.arch.regs.arch_pc
+        # data = self.ql.mem.read(pc, size=64)
+        # self.ql.os.emit_disasm(pc, data, 32)
+        # self.ql.mem.write(buf_addr, self.fat_image[offset:offset+size])
+        # print("FAT_META ", self.ql.env["FAT_META"])
+        # print("FAT_META len", len(self.ql.env["FAT_META"]))
+        # print("data:", self.ql.env["FAT_META"][offset:offset+size])
 
-    def hi(self, a):
+        # Write fuzz data onto stack
+        self.ql.mem.write(
+            buf_addr, self.ql.env["FAT_META"][offset:offset+size])
+
+        print("!"*10, "END DISKIO READ BLOCKS")
+
+    def setup_driver_binding_start(self, a):
         print(hex(self.ql.arch.regs.arch_pc), a)
-        print("2222HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        print("!"*10, "MODULE END", a)
         if a != 0:
+            # Don't thwart subsequent modules
             return False
-        print(self.ql)
-        print("3333HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        print("!"*10, "LAST MODULE")
 
-        # FatDriverBindingStart
+        # FatDriverBindingStart (from ghidra)
         address_to_call = 0x00103658
-        self.ql.hook_address(address=address_to_call, callback=self.hi2)
-
-        # On call FatAllocateVolume
-        self.ql.hook_address(address=0x001041d2, callback=self.hi3)
+        self.ql.hook_address(address=address_to_call,
+                             callback=self.fat_binding_start)
+        # On call FatOpenDevice
+        self.ql.hook_address(address=0x001041d2, callback=self.fat_open_device)
         # After call FatAllocateVolume
-        self.ql.hook_address(address=0x001037fa, callback=self.hi4)
+        self.ql.hook_address(address=0x001037fa,
+                             callback=self.fat_allocate_volume)
         # DiskIo read blocks
-        self.ql.hook_address(address=0x001012a6, callback=self.hi5)
+        self.ql.hook_address(address=0x001012a6,
+                             callback=self.disk_io_read_blocks)
 
-        # Set pc
         # self.ql.arch.regs.arch_pc = address_to_call
-
-        # Execute the code at the desired address
         # self.ql.uc.emu_start(address_to_call, timeout=0, count=1)
+
+        # 1st arg is fat driver, 2nd is our FakeController handle
         args = [0x109140, 0x101000]
         types = (PARAM_INTN, ) * len(args)
         targs = tuple(zip(types, args))
 
         def __cleanup(ql: Qiling):
-            # Give afl this address as fuzzing end
-            print("END CLEANUP 4444HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+            # Give afl this func's address as fuzzing end
+            print("!" * 10, "END CLEANUP")
 
         cleanup_trap = self.ql.os.heap.alloc(self.ql.arch.pointersize)
         hret = self.ql.hook_address(__cleanup, cleanup_trap)
         self.ql.os.fcall.call_native(address_to_call, targs, cleanup_trap)
-        print("END 4444HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
         return True
 
-    def endend(self):
-        print("END 4444HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+    def run(self, end=None, timeout=0, mode='normal', **kwargs):
+        if mode == 'normal':
+            self.ql.os.on_module_exit.append(self.setup_driver_binding_start)
 
-    def entry(self, a):
-        print("MOOOOOOOIN", a)
-        self.ql.os.emit_context()
-        return False
-
-    def run(self, end=None, timeout=0, **kwargs):
-        self.ql.os.on_module_exit.append(self.hi)
         # self.ql.os.on_module_enter.append(self.entry)
-        # self.ql.hook_address(address=0x100412, callback=self.hi)
-
         # if end:
         #   end = callbacks.set_end_of_execution_callback(self.ql, end)
 
@@ -224,5 +221,4 @@ class EmulationManager:
             with conditional(self.coverage_file, cov_utils.collect_coverage(self.ql, 'drcov_exact', self.coverage_file)):
                 self.ql.run(end=end, timeout=timeout)
         except fault.ExitEmulation:
-            # Exit cleanly.
             pass
--- core/FuzzingManager.py
+++ core/FuzzingManager.py
@@ -18,7 +18,7 @@ def start_afl(_ql: Qiling, user_data):
     """
     Callback from inside
     """
-    print("START AFL ***********************")
+    print("!"*10, "START AFL")
 
     # (varname, infile) = user_data
     infile = user_data
@@ -36,14 +36,14 @@ def start_afl(_ql: Qiling, user_data):
         """
         Informs AFL that a certain condition should be treated as a crash.
         """
-        print("AFL VALIDATE CRASH *******************************")
+        print("!"*10, "AFL VALIDATE CRASH")
         print(hex(_ql.arch.regs.arch_pc))
         return False
         if hasattr(_ql.os.heap, "validate"):
-            print("AFL IS VALIDATE CRASH *******************************")
+            print("!"*10, "AFL IS VALIDATE CRASH")
             if not _ql.os.heap.validate():
-                print("AFL HEAP INVALID *******************************")
-                # Canary was corrupted.
+                print("!"*10, "AFL HEAP INVALID")
+                # Canary was corrupted
                 verbose_abort(_ql)
                 return True
 
@@ -51,13 +51,10 @@ def start_afl(_ql: Qiling, user_data):
             err.errno != UC_ERR_OK)
         return crash
 
-    # Choose the function to inject the mutated input to the emulation environment,
-    # based on the fuzzing mode.
+    # Inject mutated FAT images through this callback
     place_input_callback = place_input_callback_fat_meta
 
-    # We start our AFL forkserver or run once if AFL is not available.
-    # This will only return after the fuzzing stopped.
-    print("START AFL BEFORE TRY FUZZ *********************")
+    print("!"*10, "START AFL BEFORE TRY")
     try:
         if not _ql.uc.afl_fuzz(input_file=infile,
                                place_input_callback=place_input_callback,
@@ -67,7 +64,7 @@ def start_afl(_ql: Qiling, user_data):
             print("Dry run completed successfully without AFL attached.")
             os._exit(0)  # that's a looot faster than tidying up.
     except unicornafl.UcAflError as ex:
-        print("!!!!!!!!!!!!!!! EXCEPT AFL ERROR", ex)
+        print("!"*10, "EXCEPT AFL ERROR", ex)
         if ex != unicornafl.UC_AFL_RET_CALLED_TWICE:
             raise
 
@@ -97,19 +94,18 @@ class FuzzingManager(EmulationManager):
     def setup_fuzz_target(self, modules_left):
         if modules_left != 0:
             return False
+
         # FatDriverBindingStart
-        # target = self.ql.loader.images[-1].path
-        # pe = pefile.PE(target, fast_load=True)
-        # image_base = self.ql.loader.images[-1].base
-        # address_to_call = image_base + 0x658  # 0x00103658
-        address_to_call = 0x00103658
-        # self.ql.hook_address(address=address_to_call, callback=self.hi2)
+        target = self.ql.loader.images[-1].path
+        pe = pefile.PE(target, fast_load=True)
+        image_base = self.ql.loader.images[-1].base
+        address_to_call = image_base + 0x658  # 0x00103658
 
         # DiskIo read blocks
         self.ql.hook_address(address=0x001012a6,
                              callback=self.disk_io_read_blocks)
 
-        # We want AFL's forkserver to spawn new copies starting from here
+        # AFL's forkserver will spawn new copies from here
         self.ql.hook_address(callback=start_afl, address=address_to_call, user_data=(
             '/blah/dummy_file'))
 
@@ -118,67 +114,21 @@ class FuzzingManager(EmulationManager):
         targs = tuple(zip(types, args))
 
         def __cleanup(ql: Qiling):
-            # Give afl this address as fuzzing end
-            print("END CLEANUP 4444HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+            # Give afl this address as fuzzing end address
+            print("!"*10, "END CLEANUP")
 
         cleanup_trap = self.ql.os.heap.alloc(self.ql.arch.pointersize)
         hret = self.ql.hook_address(__cleanup, cleanup_trap)
+
+        # Call the driver start func and invoke the fuzzer (it's hooked on that address)
         self.ql.os.fcall.call_native(address_to_call, targs, cleanup_trap)
-        print("END 4444HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        print("!"*10, "END FUZZ")
         return True
 
-    def disk_io_read_blocks(self, m):
-        # addr start in r8
-        # size param is in r9
-        # &buf pointer is in RSP+0x28
-
-        print("!!!!!!!!!!!!!!! DISK IO READ BLOCKS")
-        offset = self.ql.arch.regs.r8
-        size = self.ql.arch.regs.r9
-        buf_addr = self.ql.mem.read(
-            self.ql.arch.regs.rsp + 0x28, 8)
-        buf_addr = int.from_bytes(buf_addr, 'little')
-        # self.ql.mem.read(self.ql.arch.stack_read(
-        # 5 * self.ql.arch.pointersize), 8)
-        print("offset:", hex(offset))
-        print("size:", size)
-        print("buf addr:", buf_addr, hex(buf_addr))
-        # self.ql.os.emit_context()
-        self.ql.os.emit_stack(12)
-        pc = self.ql.arch.regs.arch_pc
-        data = self.ql.mem.read(pc, size=64)
-        self.ql.os.emit_disasm(pc, data, 32)
-        # print(self.fat_image[:size])
-        # bytes([1, 2, 3, 4]))
-        # self.ql.mem.write(buf_addr, self.fat_image[offset:offset+size])
-        print("HEREEEEEE BEFORE STACK WRITE")
-        print("FAT_META ", self.ql.env["FAT_META"])
-        print("FAT_META len", len(self.ql.env["FAT_META"]))
-        print("data:", self.ql.env["FAT_META"][offset:offset+size])
-        self.ql.mem.write(
-            buf_addr, self.ql.env["FAT_META"][offset:offset+size])
-        # self.ql.os.emit_stack(12)
-        print("HEREEEEEE AFTER STACK WRITE")
-
     def fuzz(self, end=None, timeout=0, **kwargs):
-        print("AFL FUZZ START *******************************")
-
-        load_fat_image("/home/wj/temp/uefi/test-fat/dummy_esp.img")
+        print("*"*10, "AFL FUZZ START")
 
         self.ql.os.on_module_exit.append(self.setup_fuzz_target)
 
-        print("AFL BEFORE QILING RUN **********************")
-        try:
-            # Don't collect coverage information unless explicitly requested by the user.
-            with conditional(self.coverage_file, cov_utils.collect_coverage(self.ql, 'drcov_exact', self.coverage_file)):
-                self.ql.run(end=end, timeout=timeout)
-        except fault.ExitEmulation:
-            # Exit cleanly.
-            pass
-
-        # super().run(end, timeout)
-
-
-def load_fat_image(path):
-    with open(path, "rb") as file:
-        FAT_IMAGE = file.read()
+        # Invoke all module entrypoints, the fuzzer will be started after the last module's entrypoint returns (through the `on_module_exit` hook above)
+        self.run(end, timeout, 'fuzz')
