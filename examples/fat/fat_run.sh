176917553ff5b990b08b1eca75279330eec09872
wj <wzggUaPFt5J7f@gmx.de>
2023-06-25 23:17:08 +0200

    Update module paths in fat fuzz

--- examples/fat/fat_run.sh
+++ examples/fat/fat_run.sh
@@ -2,4 +2,4 @@
 
 rm -rf afl_outputs_fat_meta
 afl-fuzz -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -U -- \
-python3 ../../efi_fuzz.py fuzz /home/wj/temp/uefi/image/FatDxeNew.efi -v nvram.pickle --extra-modules /home/wj/temp/uefi/file-system-modules/EnglishDxe.efi /home/wj/temp/uefi/file-system-modules/FakeController.efi -v ../../nvram.pickle fat @@
+python3 ../../efi_fuzz.py fuzz modules/FatDxeNew.efi -v nvram.pickle --extra-modules modules/EnglishDxe.efi modules/FakeController.efi -v ../../nvram.pickle fat @@
