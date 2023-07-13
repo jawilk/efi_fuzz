7a6251ec710bbd5e926eafae003c0a7293c5eeaf
wh57gf_-h6j <j.wilkening@campus.tu-berlin.de>
2023-07-14 00:15:58 +0200

    Enable afl parallelization

---
+++ examples/fat/fat_run_main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf afl_outputs_fat_meta
+afl-fuzz -f -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -M fuzzer01 -U -- \
+python3 ../../efi_fuzz.py fuzz modules/FatDxeNew.efi -v nvram.pickle --extra-modules modules/EnglishDxe.efi modules/FakeController.efi -v ../../nvram.pickle fat @@
--- examples/fat/fat_run_secondary.sh
+++ examples/fat/fat_run_secondary.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rm -rf afl_outputs_fat_meta
-afl-fuzz -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -U -- \
+afl-fuzz -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -S fuzzer0$1 -U -- \
 python3 ../../efi_fuzz.py fuzz modules/FatDxeNew.efi -v nvram.pickle --extra-modules modules/EnglishDxe.efi modules/FakeController.efi -v ../../nvram.pickle fat @@
