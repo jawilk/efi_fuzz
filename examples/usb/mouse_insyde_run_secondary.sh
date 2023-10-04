98f71ffb4bed38284bdae82cfe121778bd85bb45
jawilk <wzggUaPFt5J7f@gmx.de>
2023-10-04 22:54:39 +0200

    Update protocols

--- examples/usb/mouse_insyde_run_secondary.sh
+++ examples/usb/mouse_insyde_run_secondary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-afl-fuzz -D -p explore -i ../../afl_inputs_mouse_insyde -o afl_outputs_mouse_insyde -S fuzzer0$1 -U -- \
+afl-fuzz -D -p explore -i ../../afl_inputs_usb -o afl_outputs_mouse_insyde -S fuzzer0$1 -U -- \
 python3 ../../efi_fuzz.py fuzz modules/UsbMouseDxe_body.efi -v nvram.pickle -v ../../nvram.pickle fat @@
