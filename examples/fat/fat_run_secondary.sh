d31c1835f9b70e8c36d8f2ee1fb9ef11ee738f83
jawilk <wzggUaPFt5J7f@gmx.de>
2023-10-24 00:41:11 +0200

    Update FAT fuzzer

--- examples/fat/fat_run_secondary.sh
+++ examples/fat/fat_run_secondary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-afl-fuzz -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -t 40000 -S fuzzer0$1 -U -- \
+afl-fuzz -i ../../afl_inputs_fat_meta -o afl_outputs_fat_meta -t 4000 -S fuzzer0$1 -U -- \
 python3 ../../efi_fuzz.py fuzz modules/Fat_noinline.efi -v nvram.pickle --extra-modules modules/EnglishDxe.efi -v ../../nvram.pickle fat @@
